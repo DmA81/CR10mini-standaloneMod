2df19c34be78b7408f31a203ec1302e7b5243fe0
DmA81 <dario.mango@gmail.com>
2022-08-14 12:42:30 +0200

    tuned ESTEPS, LIN_ADVANCE

--- config/marlin-2.0/Configuration.h
+++ config/marlin-2.0/Configuration.h
@@ -1168,7 +1168,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.80, 79.80, 400, 431.3 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.80, 79.80, 400, 440.1 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
